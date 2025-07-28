b9787678e91783429645513c741178d010a4130d
dpetrukh <dinispetrux@gmail.com>
2025-07-28 18:29:05 +0100

    i hate this, again

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -5,14 +5,33 @@
 service mysql start
 
 
-echo "CREATE DATABASE IF NOT EXISTS $db1_name ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$db1_user'@'%' IDENTIFIED BY '$db1_pwd' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $db1_name.* TO '$db1_user'@'%' ;" >> db1.sql
+echo "CREATE DATABASE IF NOT EXISTS $SSSQL_DATABASE ;" > db1.sql
+echo "CREATE USER IF NOT EXISTS '$SSQL_USER'@'%' IDENTIFIED BY '$SSQL_SUN' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON $SSQL_DATABASE.* TO '$SSQL_USER'@'%' ;" >> db1.sql
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
 
 mysql < db1.sql
 
-# kill $(cat /var/run/mysqld/mysqld.pid)
+kill $(cat /var/run/mysqld/mysqld.pid)
+
+
+# --------------------------------
+
+# mysql -e "CREATE DATABASE IF NOT EXISTS \`${SSSQL_DATABASE}\`;"
+
+# mysql -e "CREATE USER IF NOT EXISTS \`${SSQL_USER}\`@'localhost' IDENTIFIED BY '${SSQL_SUN}';"
+
+# mysql -e "GRANT ALL PRIVILEGES ON \`${SSQL_DATABASE}\`.* TO \`${SSQL_USER}\`@'%' IDENTIFIED BY '${SSQL_SUN}';"
+
+# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SSQL_ROOT_SUN}';"
+
+# mysql -e "FLUSH PRIVILEGES;"
+
+# mysqladmin -u root -p$SSQL_ROOT_SUN shutdown
+
+# exec mysqld_safe
+
+
 
 mysqld
